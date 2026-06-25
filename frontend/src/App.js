e53bd78dafc121395c4b287344ea2fcd7f57ceec
siddardhareddy <siddardhareddy456@gmail.com>
2026-06-25 06:07:26 +0000

    updated ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const employee = { name, designation };
 
     try {
-      const response = await fetch('http://172.31.228.22:9090/api/employees', {
+      const response = await fetch('http://13.232.174.65:9090/api/employees', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
